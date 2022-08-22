ee28e35195d8e4d04f031c818bb00cb7270259bb
Orlando <carlos.orlando.rios@gmail.com>
2022-08-21 21:29:15 -0500

    trying to still debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y6=^mm%di14(_$cj2t@ndaet(ogrujo08w$u9==4c8cgnww^v*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'orlandorios.pythonanywhere.com']
 
 
 # Application definition
